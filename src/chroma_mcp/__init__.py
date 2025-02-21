4c35c4c9a88beaa4706666e57a0931c648ec93ea
Jai Radhakrishnan <jairad26@gmail.com>
2025-02-20 20:45:38 -0800

    release v0.1.9

--- src/chroma_mcp/__init__.py
+++ src/chroma_mcp/__init__.py
@@ -5,9 +5,6 @@ from . import server
 def main():
     server.main()
     
-if __name__ == "__main__":
-    main()
-
-
+    
 # Optionally expose other important items at package level
 __all__ = ["main", "server"]
